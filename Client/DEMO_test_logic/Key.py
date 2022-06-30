fd8b89ae8762fece2b691ed42a1c99e8c782c1fe
Josh Pereira <josh@Joshs-MacBook-Pro.local>
2022-06-29 23:48:12 -0700

    edits

--- Client/DEMO_test_logic/Key.py
+++ Client/DEMO_test_logic/Key.py
@@ -1,7 +1,7 @@
 class TestCase():
     def __init__(self, inputs: list, expected_output, number : int, assignment_id: int):
-        self.inputs = inputs
-        self.outputs = expected_output
+        self.inputs = self.transform_inputs(inputs)
+        self.outputs = self.transform_output(expected_output)
         self.num = number
         self.id = assignment_id
 
@@ -10,6 +10,64 @@ class TestCase():
 
     def get_outputs(self):
         return self.outputs
+
+    def transform_inputs(self, inputs: str):
+        if len(inputs) == 0:
+            raise Exception('No input variables found.')
+
+        for i in range(len(inputs)):
+            if inputs[i].isdigit():
+                inputs[i] = int(inputs[i])
+
+            elif inputs[i].lower == 'false':
+                inputs[i] == False
+
+            elif inputs[i].lower == 'True':
+                inputs[i] == True 
+
+            elif inputs[i][0] == '[' or inputs[i][-1] == ']':
+                inputs[i] = inputs[i][1:len(inputs[i])-1].split(',')
+
+            elif inputs[i][0] == '(' or inputs[i][-1] == ')':
+                inputs[i] = tuple(inputs[i][1:len(inputs[i])-1].split(','))
+
+            elif inputs[i][0] == '{' or inputs[i][-1] == '}':
+                raise TypeError('Dictionary detected. No support available. please choose a new data type.')
+
+            else:
+                try:
+                    inputs[i] = float(inputs[i])
+
+                except:
+                    pass
+
+        return inputs
+
+    def transform_output(self, output):
+        if output.isdigit():
+            return int(output)
+
+        elif output.lower() == 'false':
+            return False
+
+        elif output.lower() == 'true':
+            return True 
+
+        elif output[0] == '[' or output[-1] == ']':
+            return output[1:len(output)-1].split(',')
+
+        elif output[0] == '(' or output[-1] == ')':
+           return tuple(output[1:len(output)-1].split(','))
+
+        elif output[0] == '{' or output[-1] == '}':
+            raise TypeError('Dictionary detected. No support available. please choose a new data type.')
+
+        else:
+            try:
+                output = float(output)
+
+            except:
+                print('Sequence of "_" separated strings detected.')
 
     def __repr__(self):
         return (f'This test case supplies {self.get_inputs} as inputs and has an expected output of {self.get_outputs}.')
@@ -56,7 +114,7 @@ class Key():
         with open(filename, newline='') as file:
             if self.readable:
                 print(f'File opened.')
-            reader = csv.reader(file)
+            reader = csv.reader(file,delimiter='\t')
             data = list(reader)
 
         for i in range(1,len(data)):
@@ -79,22 +137,23 @@ class Key():
 
                 problem_tracker += 1
 
-                inputs = data[i][3].split(',')
+                inputs = data[i][3].split('_')
                 output = data[i][4]
 
-                tc = TestCase(inputs, output, data[i][2], self.get_id())
-                problem = Problem([tc],data[i][1], self.get_id())
+                tc = TestCase(inputs, output, int(data[i][2]), self.get_id())
+                problem = Problem([tc],int(data[i][1]), self.get_id())
 
                 DATA[0].append(problem)
 
             elif data[i][1] == '' and data[i][2] != 1:
                 if self.readable:
                     print('New TestCase found!')
-                inputs = data[i][3].split(',')
+                
+                inputs = data[i][3].split('_')
                 output = data[i][4]
 
-                tc = TestCase(inputs, output, data[i][2], self.get_id())
-                
+                tc = TestCase(inputs, output, int(data[i][2]), self.get_id())
+
                 DATA[0][-1].add_tc(tc)
 
         if self.readable:
