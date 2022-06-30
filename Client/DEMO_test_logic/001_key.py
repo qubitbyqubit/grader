575d76804600e6c8eadae5dd88d65f554ce55e06
Josh Pereira <josh@Joshs-MacBook-Pro.local>
2022-06-29 21:57:06 -0700

    finished demo

--- Client/DEMO_test_logic/001_key.py
+++ Client/DEMO_test_logic/001_key.py
@@ -1,18 +1,3 @@
 # This is the answer key for "Day 5: Leet Code Challenge".
-#
-# So there isn't actually any real useful data here yet. But, here's the idea: this file  will hold data 
-# about the answers to its respective assignment using the following data structure hirearchy:
-# 
-# If you're lost, say the following lines out loud and replace --> with "holds"
-#
-# Answer_Key --> Problem --> Test_Case --> input
-#                                      --> expected_output
-#
-# Here's an example:
-# day5lc_key --> problem1 --> tc1 --> {...} # dictionary of [key : value] of var_name : var_val
-#                                 --> "Yes" 
-#            --> problem2 --> tc1 --> {...} # dictionary of [key : value] of var_name : var_val
-#                                 --> 4
-
-from DEMO_test_logic import Key
-key_001 = Key.Key('/Client/DEMO_test_logic/001.csv')
+import Key as key
+answer_key = key.Key('001.csv', True)
--- Client/DEMO_test_logic/Key.py
+++ Client/DEMO_test_logic/Key.py
@@ -1,7 +1,9 @@
 class TestCase():
-    def __init__(self, inputs: list, expected_output):
+    def __init__(self, inputs: list, expected_output, number : int, assignment_id: int):
         self.inputs = inputs
         self.outputs = expected_output
+        self.num = number
+        self.id = assignment_id
 
     def get_inputs(self):
         return self.inputs
@@ -9,33 +11,110 @@ class TestCase():
     def get_outputs(self):
         return self.outputs
 
-    def __str__(self):
+    def __repr__(self):
         return (f'This test case supplies {self.get_inputs} as inputs and has an expected output of {self.get_outputs}.')
 
 class Problem():
-    def __init__(self, testcases: list[TestCase], number : int):
+    def __init__(self, testcases: list[TestCase], number : int, assignment_id: int):
         self.testcases = testcases
         self.number = number
+        self.id = assignment_id
 
     def get_testcases(self):
-        return self.testcases
+        return self.testcases[:]
 
     def get_number(self):
         return self.number
 
-    def __str__(self):
-        return (f'This is problem {self.get_number()}.')
+    def add_tc(self, tc: TestCase):
+        if type(tc) != TestCase:
+            raise TypeError('You can only add testcases!')
+
+        else:
+            testcases = self.get_testcases()
+            testcases.append(tc)
+            
+            self.testcases = testcases
+
+    def __repr__(self):
+        return (f'This is problem {self.get_number()} for assignment {self.id} with {len(self.get_testcases())} test case(s)')
 
 class Key():
-    def __init__(self, filename: str):
+    def __init__(self, filename: str, readable=False):
+        self.readable = readable
         DATA = self.load_information(filename)
         self.problems = DATA[0]
-        self.id = DATA[1]
 
-    def load_information(self, filename: str) -> tuple:
-        # TODO
-        raise NotImplementedError
+    def load_information(self, filename: str) -> list:
 
+        if self.readable:
+            print(f'Now processing the answer key with filename {filename}.')
+
+        DATA = [[], None]
+        import csv
+
+        with open(filename, newline='') as file:
+            if self.readable:
+                print(f'File opened.')
+            reader = csv.reader(file)
+            data = list(reader)
+
+        for i in range(1,len(data)):
+            problem_tracker = 0
+            
+            if self.readable:
+                print(f'Now processing {data[i]}.')
+            
+            if i == 1:
+                if self.readable:
+                    print(f'Found assignment_id: {data[i][0]}.')
+
+                DATA[1] = data[i][0]
+                self.id = data[i][0]
+
+            if data[i][1] != '' and data[i][2] == '1':
+                
+                if self.readable:
+                    print('New Problem detected!')
+
+                problem_tracker += 1
+
+                inputs = data[i][3].split(',')
+                output = data[i][4]
+
+                tc = TestCase(inputs, output, data[i][2], self.get_id())
+                problem = Problem([tc],data[i][1], self.get_id())
+
+                DATA[0].append(problem)
+
+            elif data[i][1] == '' and data[i][2] != 1:
+                if self.readable:
+                    print('New TestCase found!')
+                inputs = data[i][3].split(',')
+                output = data[i][4]
+
+                tc = TestCase(inputs, output, data[i][2], self.get_id())
+                
+                DATA[0][-1].add_tc(tc)
+
+        if self.readable:
+            print(f'Parsing complete.')
+            print(f'Did you expect the following? (y,n)')
+            print(DATA[0])
+
+            ver = input()
+
+            if ver.lower() != 'y':
+                raise Exception('Parsing Unsuccessful. Something went wrong...')
+
+            else:
+                print(f'Loading successful. Ending function.')
+
+        return DATA
+
+    def get_id(self):
+        return self.id
+        
     def __str__(self):
         return (f'This is the key data for assignment {self.id}')
 
