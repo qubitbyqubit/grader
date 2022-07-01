ae2280fd02241cb7d092470a2394f8c22292c15a
Phil Labrum <philip.labrum@gmail.com>
2022-07-01 13:03:15 -0400

    example_return included

--- Server/app.py
+++ Server/app.py
@@ -1,19 +1,48 @@
 from flask import Flask, request
 import json 
 import pickle
-import jsonpickle
+import base64
+
 app = Flask(__name__)
 
+example_return = {"notebook_id": True, 
+  "student_id": True, 
+  "problems": [
+    {"problem_number": 1, 
+      "variable_name": "var1", 
+      "checking_type": "test_case", 
+      "checking_data": [
+        {"input": "III", "output": 3}, 
+        {"input": "LVIII", "output": 58}, 
+        {"input": "MCMXCIV", "output": 1994}]
+    }, 
+    {"problem_number": 2, 
+      "variable_name": "var2", 
+      "checking_type": "equality_check", 
+      "checking_data": [
+        {"solution": ["this", "is", "one", "solution"]}, 
+        {"solution": ["this", "is", "another", "solution"]}]
+    }
+    ]
+}
 
-@app.get('/')
-def index():
-	return "nothing"
+@app.post('/<notebook_id>/<student_id>')
+def index(notebook_id, student_id):
+	student_id_dict = {"0":"John", "1":"Bill","2":"Lucy","100":"Mary"}
+	notebook_id_set = {"001", "002", "003"}
+	valid_student = (student_id in student_id_dict)
+	valid_notebook = (notebook_id in notebook_id_set)
+	data = json.dumps(example_return)
+	return data
 
 @app.post('/check_answers/')
 def check_answers():
 	data = request.form['req']
-	dic = json.loads(data)
-	print(dic)
+	decoded_pickle = base64.b64decode(data)
+	new_func = pickle.loads(decoded_pickle)
+	print(type(new_func))
+	# dic = json.loads(data)
+	# print(dic)
 
 	response = {
 		"response": "Success"
@@ -36,5 +65,8 @@ def verify_student(student_id):
 	if student_id in student_id_dict:
 		return student_id_dict[student_id]
 	else:
-		return None
+		return
 
+# Debug mode for local development
+if __name__ == "__main__":
+	app.run(debug=True)
